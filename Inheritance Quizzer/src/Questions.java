59cbecbdadd12dc1813f9ed2aec8c4decc366248
maryyoung612 <mary.young@MU-C1-07.mullen.pvt.k12.co.us>
2016-01-20 13:01:08 -0700

    done

--- Inheritance Quizzer/src/Questions.java
+++ Inheritance Quizzer/src/Questions.java
@@ -7,35 +7,31 @@ public class Questions
 	static int counter1 =0;
 		public static void genQuestions()
 			{
-		    
 			q.add(new GetterSetter("Animal a =new Animal","a.eats();", true));//1
 			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//2
-			
-			/*q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//3
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//4
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//5
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//6
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//7
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//8
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//9
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//10
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//11
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//12
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//13
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//14
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//15
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//16
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//17
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//18
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//19
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//20
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//21
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//22
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//23
-			q.add(new GetterSetter("Mammal m =new Animal","m.eats();", false));//24*/
+			q.add(new GetterSetter("Bird b =new Bird","b.bearsYoung();", true));//3
+			q.add(new GetterSetter("Mammal m =new Mammal","m.bearsYoung();", true));//4
+			q.add(new GetterSetter("Does this code work?"," ", true));//5
+			q.add(new GetterSetter("Animal a =new Animal","a.bearsYoung();", false));//6
+			q.add(new GetterSetter("FlightBehavior f =new Animal","f.flies();", false));//7
+			q.add(new GetterSetter("Animal a =new Animal","a.makesNoise(5);", false));//8
+			q.add(new GetterSetter("Bird b =new Mammal","b.eats();", false));//9
+			q.add(new GetterSetter("Mammal m =new Mammal","m.eats();", true));//10
+			q.add(new GetterSetter("Public interface FlightBehavior { public void flies() ","{*some code here*} }", false));//11
+			q.add(new GetterSetter("Mammal m =new Animal","m.hasHair();", true));//12
+			q.add(new GetterSetter("Animal a =new Bird","a.eats();", true));//13
+			q.add(new GetterSetter("Mammal m =new Animal","m.bearsYoung();", false));//14
+			q.add(new GetterSetter("Bird b =new Bird","b.bearsYoung(2);", false));//15
+			q.add(new GetterSetter("Bird b =new Bird","myFlightBehavior= (FlightBehavior) new CanFly();", true));//16
+			q.add(new GetterSetter("Bird b =new Bird","b.eats();", true));//17
+			q.add(new GetterSetter("Mammal m =new Bird","m.bearsYoung();", false));//18
+			q.add(new GetterSetter("Mammal m =new Mammal","m.bearsYoung(0);", false));//19
+			q.add(new GetterSetter("Does the interface work?","", true));//20
+			q.add(new GetterSetter("Animal a =new Animal","a.food(5);", false));//21
+			q.add(new GetterSetter("Mammal m =new Bird","m.hasHair();", false));//22
+			q.add(new GetterSetter("Bird b =new Bird","b.hasHair();", false));//23
+			q.add(new GetterSetter("Animal a =new Animal","a.eats();", true));//24
 			//int hello=(int)(Math.random()*q.size());
-			
-			
 			}
 		public static void answerQuestion()
 		{
